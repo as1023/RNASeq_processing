0f9e6f46c519b2e93a47d68db2a8eadec6240cb3
amitbiochem04 <amitbiochem04@gmail.com>
2017-05-31 13:51:30 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,21 @@
+##submit job 
+msub test.sh
+
 cancell all job 
 mjobctl -c -w user=hd_lo149 -w acct=acct1
+showq
+checkjob <jobid>
+showq -r (check for active job)
+showq -c (check for completed job)
+canceljob <with job id >
+checkjob <job_ID> get informa on of your job
+checkjob -v -v -v <job_ID> All detailed informa on
+
+
+
+mjobctl -c <job_ID>
+mjobctl -c -w state=Idle mjobctl -c -w state=Running mjobctl -c -w state=BatchHold mjobctl -c -w user=$USER
+cancel the job (new command) cancel ALL idle jobs
+cancel ALL running jobs
+cancel ALL hold jobs
+cancel ALL your jobs!
